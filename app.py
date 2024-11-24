ea18e20ed4fbe8f11de93a58cb3f31bbbfa0d496
Fernanda <bee.fernandasilva@gmail.com>
2024-11-24 18:32:38 -0300

    Tela Ongs e uploads de fotos

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+import os
+from werkzeug.utils import secure_filename
 from config import DB_CONFIG
 
 # Inicializa a aplicação Flask
 app = Flask(__name__)
 
-# Configuração do banco de dados a partir do arquivo config.py
+# Configuração do banco de dados
 app.config['MYSQL_HOST'] = DB_CONFIG['MYSQL_HOST']
 app.config['MYSQL_USER'] = DB_CONFIG['MYSQL_USER']
 app.config['MYSQL_PASSWORD'] = DB_CONFIG['MYSQL_PASSWORD']
@@ -32,7 +34,7 @@ def home():
 @app.route('/ongs')
 def ongs():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT id_ONG, nome, cnpj, causa, cidade, estado, logo FROM ong")
+    cur.execute("SELECT id_ONG, nome, causa, cidade, estado, logo FROM ong")
     ongs = cur.fetchall()
     cur.close()
     return render_template('ongs.html', ongs=ongs)
@@ -52,7 +54,7 @@ def galeria():
 def contato():
     return render_template('contato.html')
 
-# Rota para a página de cadastro 
+# Rota para a página de cadastro
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
@@ -62,6 +64,7 @@ def cadastro():
 def login():
     return render_template('login.html')
 
+# Rota para cadastrar ONG
 @app.route('/cadastrar_ong', methods=['GET', 'POST'])
 def cadastrar_ong():
     if request.method == 'POST':
@@ -78,26 +81,26 @@ def cadastrar_ong():
         senha_ONG = request.form['senha_ONG']
 
         # Gerenciar upload do arquivo
-        logo = request.files['logo']  # Campo 'logo' vindo do formulário
+        logo = request.files['logo']
         logo_path = ''
         if logo and allowed_file(logo.filename):
             filename = secure_filename(logo.filename)
-            logo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            logo.save(logo_path)
+            logo_path = os.path.join('uploads', filename)  # Caminho relativo
+            logo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         # Inserir no banco de dados
         cur = mysql.connection.cursor()
         cur.execute("""
-            INSERT INTO ong (nome, cnpj, causa, id_Necessidade, cep, logradouro, numero, complemento, estado, cidade, senha_ONG)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (nome, cnpj, causa, id_necessidade, cep, logradouro, numero, complemento, estado, cidade, senha_ONG))
+            INSERT INTO ong (nome, cnpj, causa, id_Necessidade, cep, logradouro, numero, complemento, estado, cidade, senha_ONG, logo)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (nome, cnpj, causa, id_necessidade, cep, logradouro, numero, complemento, estado, cidade, senha_ONG, logo_path))
         mysql.connection.commit()
         cur.close()
 
         return redirect(url_for('ongs'))
-
     return render_template('login.html')
 
+# Rota para atualizar a logo de uma ONG
 @app.route('/atualizar_logo', methods=['POST'])
 def atualizar_logo():
     if request.method == 'POST':
@@ -107,8 +110,8 @@ def atualizar_logo():
         # Verifica se o arquivo enviado é válido
         if logo and allowed_file(logo.filename):
             filename = secure_filename(logo.filename)
-            logo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            logo.save(logo_path)
+            logo_path = os.path.join('uploads', filename)
+            logo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             # Atualiza o caminho da logo no banco de dados
             cur = mysql.connection.cursor()
@@ -120,11 +123,11 @@ def atualizar_logo():
             mysql.connection.commit()
             cur.close()
 
-            return redirect(url_for('ongs'))  # Redireciona para a página de ONGs
+            return redirect(url_for('ongs'))
         else:
             return "Arquivo inválido. Por favor, envie uma imagem no formato permitido (png, jpg, jpeg, gif)."
 
-
+# Rota para cadastrar voluntário
 @app.route('/cadastrar_voluntario', methods=['GET', 'POST'])
 def cadastrar_voluntario():
     if request.method == 'POST':
@@ -151,9 +154,7 @@ def cadastrar_voluntario():
         cur.close()
 
         return redirect(url_for('cadastrar_voluntario'))
-
     return render_template('login.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
